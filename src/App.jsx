006f6cc3a631da6da1dd919bd2a01d2794ca10b3
Luis Díaz <luisdh.8@outlook.com>
2025-05-19 20:42:38 -0600

    Initial commit: project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Buscador de Películas</h1>
+    </div>
+  );
+}
+
+export default App;
